7b53da6f552d45179c1f8653fdbf756e78f205a6
gajagajago <gajagajago@naver.com>
2021-04-13 13:41:42 +0900

    Temp user sign_in & sign_out

--- lib/providers/user_auth/authenticate.dart
+++ lib/providers/user_auth/authenticate.dart
@@ -32,11 +32,29 @@ class Authenticate with ChangeNotifier {
     this.authToken = null;
     await FlutterSession().set('authentication_token', '');
   }
-
+  /* 유저 로그인/회원가입 서버 개발 전 임시방편 코드
   Future signIn({Map<String, dynamic> params}) async {
     await HttpRequest()
         .post(partialUrl: "sign_in", body: params)
         .then((response) => saveAuthToken(json.decode(response.body)['authentication_token']));
+  }
+  */
+  //유저 로그인/회원가입 서버 개발 전 임시방편 코드
+  Future signIn({Map<String, dynamic> params}) async {
+    List<dynamic> tempUsers = [
+      { "email": "gajagajago@naver.com", "password": "daniel1004" },
+      { "email": "khko", "password": "123456" },
+    ];
+    print(params);
+
+    var valid = tempUsers.any((e) => e['email'] == params['email'] && e['password'] == params['password']);
+
+    if (valid) {
+      await Future.delayed(const Duration(milliseconds: 100), () => 'temp_auth_blah_blah')
+          .then((response) => saveAuthToken(response));
+    } else {
+      throw Exception('No valid user');
+    }
   }
 
   Future signUp({Map<String, dynamic> params}) async {
@@ -44,10 +62,16 @@ class Authenticate with ChangeNotifier {
         .post(partialUrl: "sign_up", body: params)
         .then((response) => saveAuthToken(json.decode(response.body)['authentication_token']));
   }
-
+  /*
   Future signOut({String authToken}) async {
     await HttpRequest()
         .delete(partialUrl: "sign_out", authToken: authToken)
         .then((response) => response);
+  }
+  */
+  // 유저 로그인/회원가입 서버 개발 전 임시방편 코드
+  Future signOut({String authToken}) async {
+    await Future.delayed(const Duration(milliseconds: 100), () => '')
+        .then((response) => saveAuthToken(response));
   }
 }
