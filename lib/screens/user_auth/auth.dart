88b0b2392792410b62d8cb82a693f60c1ac29179
gajagajago <gajagajago@naver.com>
2021-05-13 23:47:14 +0900

    iOS setup done

--- lib/screens/user_auth/auth.dart
+++ lib/screens/user_auth/auth.dart
@@ -10,18 +10,32 @@ class Auth extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             SvgPicture.asset('assets/logos/guam_logo.svg', color: HexColor('#6200EE')),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  RaisedButton(
-                    child: Text('회원가입'),
-                    onPressed: () => Navigator.pushNamed(context, '/sign_up'),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RaisedButton(
+                      child: Text('회원가입'),
+                      onPressed: () => Navigator.pushNamed(context, '/sign_up'),
+                    ),
+                    RaisedButton(
+                      child: Text('로그인'),
+                      onPressed: () => Navigator.pushNamed(context, '/sign_in'),
+                    ),
+                  ]
+                ),
+                InkWell(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    height: MediaQuery.of(context).size.height * 0.07,
+                    child: Image(
+                      image: AssetImage("assets/buttons/kakao_login_medium_narrow.png"),
+                    ),
                   ),
-                  RaisedButton(
-                    child: Text('로그인'),
-                    onPressed: () => Navigator.pushNamed(context, '/sign_in'),
-                  ),
-                ]
+                  onTap: () {},
+                )
+              ],
             )
           ],
         )
