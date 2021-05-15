0cc5bb140f33eb2c820ef57f23e8ebeb8b0e1423
yeonghyeonKO <dk02315@gmail.com>
2021-05-15 12:20:26 +0900

    Make Sort Icon toggle

--- lib/screens/projects/project_search.dart
+++ lib/screens/projects/project_search.dart
@@ -2,38 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:guam_front/commons/back.dart';
 import 'package:guam_front/screens/projects/project_search_form.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  bool isFilterOpen;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    isFilterOpen = false;
+    super.initState();
+  }
+
+  void _isFilterOpenChange() {
+    setState(() {
+      isFilterOpen = !isFilterOpen;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        /* customizing하신 appBar의 경우, text 자리에 string만 가능한
+      /* customizing하신 appBar의 경우, text 자리에 string만 가능한
        상태라서 임시방편으로 AppBar 사용했습니다. */
-        appBar: AppBar(
-      backgroundColor: Colors.white,
-      leading: Back(),
-      title: Column(
-        children: <Widget>[
-          Container(
-              color: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  SearchForm(),
-                  Padding(padding: EdgeInsets.fromLTRB(10, 0, 0, 0)),
-                  IconButton(
-                      icon: Icon(
-                        Icons.sort,
-                        color: Colors.black,
-                        size: 30,
-                      ),
-                      onPressed: () {
-                        /* sort 아이콘 클릭 시 Search Bar 하단에
-                        정렬도구들을 담은 Container가 붙어야함.*/
-                      })
-                ],
-              )),
-          // _buildBody(context)
-        ],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Back(),
+        title: Column(
+          children: <Widget>[
+            Container(
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    SearchForm(),
+                    IconButton(
+                        icon: Icon(
+                          Icons.sort,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                        onPressed: _isFilterOpenChange)
+                  ],
+                )),
+            // _buildBody(context)
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
