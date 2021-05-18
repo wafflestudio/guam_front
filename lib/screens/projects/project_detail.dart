0299cd3d100a93cf418fad2a87cadebd1fe372c8
yeonghyeonKO <dk02315@gmail.com>
2021-05-18 16:03:06 +0900

    fix layout for apply status and attach percent bar

--- lib/screens/projects/project_detail.dart
+++ lib/screens/projects/project_detail.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 import '../../commons/app_bar.dart';
 import '../../commons/back.dart';
 import '../../models/project.dart';
-import '../../main.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class DetailProject extends StatelessWidget {
   final Project project;
@@ -26,14 +27,14 @@ class DetailProject extends StatelessWidget {
             style: TextStyle(fontSize: 15, color: Colors.black45),
           ),
           Container(
+            height: 24,
             alignment: Alignment.centerLeft,
             child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  width: 32,
-                  height: 32,
-                  child: Icon(Icons.person, color: Colors.white, size: 30),
+                  width: 24,
+                  height: 24,
+                  child: Icon(Icons.person, color: Colors.white, size: 24),
                   decoration: BoxDecoration(
                       boxShadow: [BoxShadow(color: Colors.blue)],
                       shape: BoxShape.circle),
@@ -43,7 +44,7 @@ class DetailProject extends StatelessWidget {
                 ),
                 Text(
                   "waffle0112",
-                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  style: TextStyle(fontSize: 12, color: Colors.black),
                 ),
               ],
             ),
@@ -53,173 +54,114 @@ class DetailProject extends StatelessWidget {
               alignment: Alignment.centerLeft,
               child: Text(
                 project.title,
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               )),
           Container(
               padding: EdgeInsets.fromLTRB(5, 20, 5, 30),
               alignment: Alignment.centerLeft,
               child: Text(
                 project.description ?? 'default description',
-                style: TextStyle(fontSize: 17),
+                style: TextStyle(fontSize: 14),
               )),
           Container(
-            height: size.height * 0.05,
-            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+            height: 38,
+            padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
             decoration: BoxDecoration(
               color: Colors.grey[300],
               borderRadius: BorderRadius.all(
-                Radius.circular(15),
+                Radius.circular(30),
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey[400],
-                  blurRadius: 5.0,
-                  spreadRadius: 1.0,
-                ),
-                BoxShadow(
-                  color: Colors.white,
-                  blurRadius: 10.0,
-                  spreadRadius: 1.0,
-                ),
-              ],
             ),
             child: Row(children: <Widget>[
               Icon(Icons.timer),
               Text(' 진행 기간    ',
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
               Text(
                 '${project.time}주',
-                style: TextStyle(fontSize: 17),
+                style: TextStyle(fontSize: 14),
               )
             ]),
           ),
           SizedBox(height: size.height * 0.03),
           Container(
-            height: size.height * 0.275,
-            padding: EdgeInsets.fromLTRB(10, 10, 0, 10),
+            height: size.height * 0.25,
+            padding: EdgeInsets.fromLTRB(15, 20, 0, 0),
             decoration: BoxDecoration(
               color: Colors.grey[300],
               borderRadius: BorderRadius.all(
-                Radius.circular(15),
+                Radius.circular(30),
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey[400],
-                  blurRadius: 10.0,
-                  spreadRadius: 1.0,
-                ),
-                BoxShadow(
-                  color: Colors.white,
-                  blurRadius: 10.0,
-                  spreadRadius: 1.0,
-                ),
-              ],
             ),
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Row(
                     children: [
-                      Icon(Icons.people),
+                      Icon(Icons.people, size: 20),
                       Text(' 참여 현황 ',
                           style: TextStyle(
-                              fontSize: 17, fontWeight: FontWeight.bold)),
+                              fontSize: 14, fontWeight: FontWeight.bold)),
                     ],
                   ),
-                  DataTable(
-                    columnSpacing: size.width * 0.05,
-                    dividerThickness: 0,
-                    columns: [
-                      DataColumn(
-                          label: Text('포지션',
-                              style: TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold))),
-                      DataColumn(
-                          label: Text('기술 스택',
-                              style: TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold))),
-                      DataColumn(
-                          label: Text('참가인원',
-                              style: TextStyle(
-                                  color: Colors.black54,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold))),
-                      DataColumn(label: Text('')),
-                    ],
-                    rows: [
-                      DataRow(cells: [
-                        DataCell(Text('백엔드')),
-                        DataCell(Text('${project.backFramework}')),
-                        DataCell(Container(
-                            alignment: Alignment.topRight,
-                            width: size.width * 0.15,
-                            height: size.height * 0.025,
-                            padding: EdgeInsets.only(right: 6, top: 2),
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(20),
-                              ),
-                            ),
-                            child: Text('${project.backHeadCount}',
-                                style: TextStyle(
-                                    fontSize: 15, color: Colors.white)))),
-                        DataCell(Radio(
-                          value: 1,
-                          groupValue: 0,
-                          onChanged: (_) {},
-                        ))
-                      ]),
-                      DataRow(cells: [
-                        DataCell(Text('프론트엔드')),
-                        DataCell(Text('${project.frontFramework}')),
-                        DataCell(Container(
-                            alignment: Alignment.topRight,
-                            width: size.width * 0.15,
-                            height: size.height * 0.025,
-                            padding: EdgeInsets.only(right: 6, top: 2),
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(20),
-                              ),
-                            ),
-                            child: Text('${project.frontHeadCount}',
-                                style: TextStyle(
-                                    fontSize: 15, color: Colors.white)))),
-                        DataCell(Radio(
-                          value: 0,
-                          groupValue: 0,
-                          onChanged: (_) {},
-                        ))
-                      ]),
-                      DataRow(cells: [
-                        DataCell(Text('디자이너')),
-                        DataCell(Text('상관 없음')),
-                        DataCell(Container(
-                            alignment: Alignment.topRight,
-                            width: size.width * 0.15,
-                            height: size.height * 0.025,
-                            padding: EdgeInsets.only(right: 6, top: 2),
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(20),
-                              ),
-                            ),
-                            child: Text('1',
-                                style: TextStyle(
-                                    fontSize: 15, color: Colors.white)))),
-                        DataCell(Radio(
-                          value: 1,
-                          groupValue: 0,
-                          onChanged: (_) {},
-                        ))
-                      ]),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            _columnName("포지션"),
+                            _text("백엔드"),
+                            _text("프론트엔드"),
+                            _text("디자니어"),
+                          ],
+                        ),
+                        width: size.width * 0.2,
+                        height: size.height * 0.2,
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      ),
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            _columnName("기술 스택"),
+                            _text(project.backFramework),
+                            _text(project.frontFramework),
+                            _text("상관 없음"),
+                          ],
+                        ),
+                        width: size.width * 0.25,
+                        height: size.height * 0.2,
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      ),
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            _columnName("참가 인원"),
+                            _percentBar(2, 4),
+                            _percentBar(1, 3),
+                            _percentBar(1, 1)
+                          ],
+                        ),
+                        width: size.width * 0.2,
+                        height: size.height * 0.2,
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      ),
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            _columnName(""),
+                            _radioCheckButton(),
+                            _radioCheckButton(),
+                            _radioCheckButton(),
+                          ],
+                        ),
+                        width: size.width * 0.2,
+                        height: size.height * 0.2,
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      ),
                     ],
                   ),
                 ]),
@@ -244,6 +186,15 @@ class DetailProject extends StatelessWidget {
     );
   }
 
+  Widget _text(String text) {
+    return Container(
+        padding: EdgeInsets.only(top: 17, bottom: 17),
+        child: Text(
+          '$text',
+          style: TextStyle(height: 1, fontSize: 14),
+        ));
+  }
+
   Widget _applyButton(Size size) {
     return Container(
       width: size.width,
@@ -257,6 +208,51 @@ class DetailProject extends StatelessWidget {
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
           onPressed: () {}),
+    );
+  }
+
+  Widget _columnName(String attribute) {
+    return Text("$attribute",
+        style: TextStyle(
+            color: HexColor("#828282"),
+            fontSize: 14,
+            fontWeight: FontWeight.bold));
+  }
+
+  Widget _radioCheckButton() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 0, 10, 0),
+      child: Radio(
+        fillColor: MaterialStateProperty.all<Color>(HexColor("#08951C")),
+        value: 0,
+        groupValue: 0,
+        onChanged: (_) {},
+      ),
+    );
+  }
+
+  Widget _percentBar(
+    int currentCount,
+    int totalCount,
+    /* response 포지션으로 묶은 dictionary 내부에 기술스택, 참가인원 전달해야함.
+     _percentBar의 인자로 포지션을 받아 각 경우에 따른 percent 정도를 산출할 예정.*/
+  ) {
+    return Padding(
+      padding: EdgeInsets.only(top: 15, bottom: 13),
+      child: LinearPercentIndicator(
+        width: 60,
+        animation: true,
+        lineHeight: 20.0,
+        animationDuration: 500,
+        percent: currentCount / totalCount,
+        center: Container(
+            padding: EdgeInsets.only(left: 37),
+            alignment: Alignment.centerRight,
+            child: Text("$totalCount",
+                style: TextStyle(fontSize: 12, color: Colors.white))),
+        backgroundColor: HexColor("#828282"),
+        progressColor: HexColor("#8CE591"),
+      ),
     );
   }
 }
