23cb770e9e758d594b25d001cbb86396abc006e7
gajagajago <gajagajago@naver.com>
2021-05-20 23:48:26 +0900

    move KakaoContext.id into kakao_login.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:provider/provider.dart';
-import 'package:kakao_flutter_sdk/all.dart';
 import 'providers/user_auth/authenticate.dart';
 import 'screens/home/root.dart';
 import 'screens/user_auth/sign_up.dart';
 import 'screens/user_auth/sign_in.dart';
 
 void main() {
-  KakaoContext.clientId = "367d8cf339e2ba59376ba647c7135dd2";
-  KakaoContext.javascriptClientId = "2edf60d1ebf23061d200cfe4a68a235a";
   runApp(MyApp());
 }
 
--- lib/screens/user_auth/kakao_login.dart
+++ lib/screens/user_auth/kakao_login.dart
@@ -4,7 +4,12 @@ import 'package:kakao_flutter_sdk/all.dart';
 
 class KakaoLogin extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => KakaoLoginState();
+  State<StatefulWidget> createState() {
+    KakaoContext.clientId = "367d8cf339e2ba59376ba647c7135dd2";
+    KakaoContext.javascriptClientId = "2edf60d1ebf23061d200cfe4a68a235a";
+
+    return KakaoLoginState();
+  }
 }
 
 class KakaoLoginState extends State<KakaoLogin> {
