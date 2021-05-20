5b3b3312df00a1fe86179d1ec02fceca80d8b4c5
gajagajago <gajagajago@naver.com>
2021-05-21 00:25:43 +0900

    init firebase app in auth

--- lib/screens/user_auth/auth.dart
+++ lib/screens/user_auth/auth.dart
@@ -2,35 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'kakao_login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class Auth extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            SvgPicture.asset('assets/logos/guam_logo.svg', color: HexColor('#6200EE')),
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    RaisedButton(
-                      child: Text('회원가입'),
-                      onPressed: () => Navigator.pushNamed(context, '/sign_up'),
-                    ),
-                    RaisedButton(
-                      child: Text('로그인'),
-                      onPressed: () => Navigator.pushNamed(context, '/sign_in'),
-                    ),
-                  ]
-                ),
-                KakaoLogin(),
-              ],
-            )
-          ],
+        body: FutureBuilder(
+          future: _initFirebaseApp(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SvgPicture.asset('assets/logos/guam_logo.svg', color: HexColor('#6200EE')),
+                  Column(
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            MaterialButton(
+                              child: Text('회원가입'),
+                              onPressed: () => Navigator.pushNamed(context, '/sign_up'),
+                            ),
+                            MaterialButton(
+                              child: Text('로그인'),
+                              onPressed: () => Navigator.pushNamed(context, '/sign_in'),
+                            ),
+                          ]
+                      ),
+                      KakaoLogin(),
+                    ],
+                  )
+                ],
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         )
     );
   }
+
+  Future<FirebaseApp> _initFirebaseApp() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
 }
+
