a13810256a79ae8d5f882d7afb419d0f1eab7fa7
gajagajago <gajagajago@naver.com>
2021-06-24 15:47:14 +0900

    Common Thread text field

--- lib/screens/boards/threads.dart
+++ lib/screens/boards/threads.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:guam_front/screens/boards/thread.dart';
-import '../../commons/grey_container.dart';
 import '../../models/boards/thread.dart' as ThreadModel;
 import 'thread.dart';
 import 'iconTitle.dart';
+import '../../commons/thread_text_field.dart';
 
 class Threads extends StatelessWidget {
   final List<ThreadModel.Thread> threads;
@@ -39,29 +39,7 @@ class Threads extends StatelessWidget {
                         physics: ClampingScrollPhysics(),
                       ),
                     ),
-                    DecoratedBox(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.white,
-                      ),
-                      child: Container(
-                        height: 36,
-                        padding: EdgeInsets.symmetric(horizontal: 6),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: TextField(
-                                decoration: null,
-                                style: TextStyle(fontSize: 12),
-                              ),
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.send_outlined),
-                              onPressed: () {}, )
-                          ],
-                        ),
-                      )
-                    )
+                    ThreadTextField(),
                   ],
                 ),
               )
@@ -70,26 +48,5 @@ class Threads extends StatelessWidget {
         ]
       )
     );
-    /*
-    return GreyContainer(
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '스레드',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          Container(
-            child: Column(
-                children: [...threads.map((e) => Thread(e))],
-            ),
-          )
-        ],
-      ),
-    );
-     */
   }
 }
