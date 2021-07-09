db0136785b23d90b5bdb30722b3324d5192acc83
yeonghyeonKO <dk02315@gmail.com>
2021-07-09 19:20:44 +0900

    fix error from position selection form

--- lib/screens/my_page/make_profile_page.dart
+++ lib/screens/my_page/make_profile_page.dart
@@ -4,7 +4,6 @@ import 'package:guam_front/commons/back.dart';
 import 'package:guam_front/commons/custom_app_bar.dart';
 import 'package:guam_front/models/profile.dart';
 import 'package:guam_front/providers/stacks/stacks.dart';
-import 'package:guam_front/screens/my_page/profile_filter_chip.dart';
 import 'package:guam_front/screens/my_page/profile_filter_value_chip.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:image_picker/image_picker.dart';
@@ -293,14 +292,7 @@ class _MakeProfilePageState extends State<MakeProfilePage> {
                   fontWeight: FontWeight.bold,
                   color: Colors.black,
                 ))),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          ...techStacks.entries.map((e) => ProfileFilterChip(
-              content: e.key,
-              display: e.key,
-              selected: selectedKey == e.key,
-              selectKey: selectKey,
-              filterValues: e.value))
-        ]),
+        positionSelection(),
         if (selectedKey != null)
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -320,6 +312,76 @@ class _MakeProfilePageState extends State<MakeProfilePage> {
             ],
           ),
       ],
+    );
+  }
+
+  void _savePosition(idx) {
+    setState(() {
+      selectedKey = techStacksInfo.keys.toList()[idx];
+    });
+  }
+
+  final isSelected = <bool>[false, false, false];
+
+  Widget positionSelection() {
+    return Container(
+      padding: EdgeInsets.only(left: 10),
+      child: ToggleButtons(
+        fillColor: HexColor("85DC40"),
+        borderColor: HexColor("000000").withOpacity(0.6),
+        borderRadius: BorderRadius.circular(10),
+        borderWidth: 0.5,
+        constraints: BoxConstraints(
+            minWidth: (MediaQuery.of(context).size.width * 0.85) / 3,
+            minHeight: 40),
+        isSelected: isSelected,
+        onPressed: (idx) {
+          setState(() {
+            for (int i = 0; i < isSelected.length; i++) {
+              isSelected[i] = i == idx;
+            }
+            _savePosition(idx);
+          });
+        },
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: Text(
+              '백엔드',
+              style: (selectedKey == '백엔드')
+                  ? TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white)
+                  : TextStyle(fontSize: 14, color: Colors.black),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: Text(
+              '프론트엔드',
+              style: (selectedKey == '프론트엔드')
+                  ? TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white)
+                  : TextStyle(fontSize: 14, color: Colors.black),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: Text(
+              '디자이너',
+              style: (selectedKey == '디자이너')
+                  ? TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white)
+                  : TextStyle(fontSize: 14, color: Colors.black),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
