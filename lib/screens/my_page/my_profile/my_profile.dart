c8191a07efe9601ff227b66429637b228a801be9
yeonghyeonKO <dk02315@gmail.com>
2021-07-16 00:10:54 +0900

    design for myTechStacks and active projects

--- lib/screens/my_page/my_profile/my_profile.dart
+++ lib/screens/my_page/my_profile/my_profile.dart
@@ -42,72 +42,144 @@ class MyProfile extends StatelessWidget {
                           topLeft: Radius.circular(30),
                         ),
                       ),
-                      child: Column(children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.fromLTRB(15, 5, 0, 0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              _startBar("000000", 100, 5),
-                              SizedBox(height: size.height * 0.015),
-                              Text('🛠 기술 스택     ',
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(15, 5, 0, 0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            _startBar("000000", 100, 5),
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 15),
+                              child: Text('🛠 기술 스택     ',
                                   style: TextStyle(
                                       fontSize: 16,
                                       fontWeight: FontWeight.bold)),
-                              Text(
-                                'ㅁㄴㅇㄹㅁㄴㅇㄹ',
-                                style: TextStyle(fontSize: 14),
-                              ),
-                              SizedBox(height: size.height * 0.02),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.symmetric(vertical: 10),
-                                    child: Text('✋ 자기 소개 ',
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold)),
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  width: 100,
+                                  padding: EdgeInsets.only(top: 15),
+                                  child: Text("백엔드"),
+                                ),
+                                Expanded(
+                                  child: _wrap(),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  width: 100,
+                                  padding: EdgeInsets.only(top: 15),
+                                  child: Text("프론트엔드"),
+                                ),
+                                Expanded(
+                                  child: _wrap(),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 10),
+                                  child: Text('✋ 자기 소개 ',
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold)),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(right: 20),
+                                  child: Container(
+                                    width: size.width,
+                                    padding:
+                                        EdgeInsets.fromLTRB(10, 10, 20, 10),
+                                    color: HexColor("#FFEB94"),
+                                    child: Text("ㅎㅇ"),
                                   ),
-                                  Padding(
-                                    padding: EdgeInsets.only(right: 20),
-                                    child: Container(
-                                      width: size.width,
-                                      padding:
-                                          EdgeInsets.fromLTRB(10, 10, 20, 10),
-                                      color: HexColor("#FFEB94"),
-                                      child: Text("ㅎㅇ"),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              SizedBox(height: size.height * 0.02),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.symmetric(vertical: 10),
-                                    child: Text('📋 참여한 프로젝트 ',
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold)),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: size.height * 0.02),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 10),
+                                  child: Text('📋 참여한 프로젝트 ',
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold)),
+                                ),
+                                SingleChildScrollView(
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(right: 5),
+                                        child: SizedBox(
+                                          width: 140,
+                                          child: ElevatedButton(
+                                            onPressed: () {},
+                                            child: Padding(
+                                              padding: EdgeInsets.only(top: 80),
+                                              child: Text(
+                                                "유튜브 제작하기",
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ),
+                                            style: ElevatedButton.styleFrom(
+                                              minimumSize: Size(140, 140),
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        20), // <-- Radius
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: SizedBox(
+                                          width: 140,
+                                          child: ElevatedButton(
+                                            onPressed: () {},
+                                            child: Padding(
+                                              padding: EdgeInsets.only(top: 80),
+                                              child: Text(
+                                                "딥러닝 스터디",
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    fontWeight:
+                                                        FontWeight.bold),
+                                              ),
+                                            ),
+                                            style: ElevatedButton.styleFrom(
+                                              minimumSize: Size(140, 140),
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        20), // <-- Radius
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  Padding(
-                                    padding: EdgeInsets.only(right: 20),
-                                    child: Container(
-                                      width: size.width,
-                                      padding:
-                                          EdgeInsets.fromLTRB(10, 10, 20, 10),
-                                      color: HexColor("#FFEB94"),
-                                      child: Text("ㅎㅇ"),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
+                                  scrollDirection: Axis.horizontal,
+                                )
+                              ],
+                            ),
+                          ],
                         ),
-                      ]),
+                      ),
                     ),
                   ]),
                 ),
@@ -119,6 +191,21 @@ class MyProfile extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+
+  Widget _wrap() {
+    return Wrap(
+      spacing: 4,
+      children: [
+        _buildChip("asdf"),
+        _buildChip("aㅁㄴㅇsdf"),
+        _buildChip("asdf"),
+        _buildChip("asㄹㅁㄴㅇdf"),
+        _buildChip("asdf"),
+        _buildChip("asㄹdf"),
+        _buildChip("asdf"),
+      ],
     );
   }
 
@@ -136,9 +223,18 @@ class MyProfile extends StatelessWidget {
     );
   }
 
-  Widget _techStackChip(String techStack) {
-    return ClipRect(
-        child: CircleAvatar(
-            child: Icon(Icons.person, color: Colors.white, size: 100)));
+  Widget _buildChip(String label) {
+    return Chip(
+      backgroundColor: HexColor("#E9E9E9"),
+      side: BorderSide(color: HexColor("#979797"), width: 0.5),
+      labelPadding: EdgeInsets.symmetric(horizontal: 2),
+      label: Text(
+        label,
+        style: TextStyle(
+          fontSize: 12,
+          color: Colors.black,
+        ),
+      ),
+    );
   }
 }
