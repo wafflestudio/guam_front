cc2a17403479c881177c02a1c02e4776e0f1c915
yeonghyeonKO <dk02315@gmail.com>
2021-07-25 10:41:12 +0900

    change Profile Image widget structure

--- lib/screens/my_page/my_profile/my_profile_top.dart
+++ lib/screens/my_page/my_profile/my_profile_top.dart
@@ -1,7 +1,5 @@
-
 import 'package:flutter/material.dart';
 import 'package:guam_front/models/profile.dart';
-import 'package:hexcolor/hexcolor.dart';
 
 class MyProfileTop extends StatelessWidget {
   final Profile me;
@@ -17,25 +15,24 @@ class MyProfileTop extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.only(top: 20),
             child: ClipRRect(
-              // borderRadius: BorderRadius.circular(100),
+                // borderRadius: BorderRadius.circular(100),
                 child: (me.imageUrl == null)
                     ? CircleAvatar(
-                    backgroundColor: Colors.black38,
-                    radius: 50,
-                    child: Icon(Icons.person,
-                        color: Colors.white, size: 90))
-                    : ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image.network(
-                        me.imageUrl))),
+                        backgroundColor: Colors.black38,
+                        radius: 50,
+                        child:
+                            Icon(Icons.person, color: Colors.white, size: 90))
+                    : CircleAvatar(
+                        backgroundColor: Colors.black38,
+                        radius: 50,
+                        child: Image.network(me.imageUrl))),
           ),
         ),
         Padding(
           padding: EdgeInsets.all(20),
           child: Text(
             me.nickname ?? "",
-            style: TextStyle(
-                fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
         ),
       ],
