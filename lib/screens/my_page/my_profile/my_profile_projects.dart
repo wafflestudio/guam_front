2c34e2b6e432f3bb77ed04e20b7fcfe509074751
yeonghyeonKO <dk02315@gmail.com>
2021-07-30 15:09:09 +0900

    decorate my project box

--- lib/screens/my_page/my_profile/my_profile_projects.dart
+++ lib/screens/my_page/my_profile/my_profile_projects.dart
@@ -31,23 +31,28 @@ class MyProfileProjects extends StatelessWidget {
                               child: Container(
                             decoration: BoxDecoration(
                                 image: DecorationImage(
+                                  colorFilter: ColorFilter.mode(
+                                      Colors.white.withOpacity(0.5),
+                                      BlendMode.dstATop),
                                   // Temp code. Should use cached_network_image with errorWidget (default image) and placeholder
                                   image:
                                       // project.thumbnail != "" ? NetworkImage(project.thumbnail) :
                                       AssetImage(
-                                          "assets/images/project-thumbnail-default.jpeg"),
+                                          "assets/images/project-thumbnail-default.png"),
                                   fit: BoxFit.fill,
                                 ),
-                                borderRadius: BorderRadius.circular(5)),
+                                borderRadius: BorderRadius.circular(20)),
                           )),
                           Positioned.fill(
                             child: Container(
                               padding: EdgeInsets.all(20),
-                              alignment: Alignment.bottomLeft,
+                              alignment: Alignment.bottomCenter,
                               child: Text(
                                 e,
                                 style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
                               ),
                             ),
                           )
