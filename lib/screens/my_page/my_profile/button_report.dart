ed29323bd730aea78e71fafb244302a529f0ec96
gajagajago <gajagajago@naver.com>
2021-08-12 22:31:37 +0900

    New buttons added

---
+++ lib/screens/my_page/my_profile/button_report.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../../../commons/buttons/common_outlined_button.dart';
+
+class ButtonReport extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return commonOutlinedButton(
+      text: "Ask Guam anything 🏝️",
+      onPressed: () {
+
+      }
+    );
+  }
+}
