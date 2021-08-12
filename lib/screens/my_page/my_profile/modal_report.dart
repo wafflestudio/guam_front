11df42f036fe18efa2e088b92592c76bff381e6d
gajagajago <gajagajago@naver.com>
2021-08-12 23:43:40 +0900

    Temp. Need Dart Mailer package

--- lib/screens/my_page/my_profile/modal_report.dart
+++ lib/screens/my_page/my_profile/modal_report.dart
@@ -7,34 +7,59 @@ class ModalReport extends StatelessWidget {
     return Wrap(
       children: [
         Material(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            width: double.infinity,
-            color: Colors.white,
-            child: Column(
-              children: [
-                Text(
-                  "문의 요청하기",
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Color.fromRGBO(129, 129, 129, 1),
-                    height: 24,
+          child: Padding(
+            padding: MediaQuery.of(context).viewInsets,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(203, 203, 203, 0.5),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.notifications_outlined,
+                          color: Color.fromRGBO(129, 129, 129, 1),
+                          size: 16,
+                        ),
+                        Text(
+                          "문의하기",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Color.fromRGBO(129, 129, 129, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                    margin: EdgeInsets.only(bottom: 10),
                   ),
-                ),
-                Text(
-                  "궁금하신 사항, 버그 등을 보내주세요!<br>문의주신 내용은 개발팀 메일로 전송됩니다.",
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.black,
-                    height: 48,
+                  Container(
+                    child: Text(
+                      "궁금하신 사항, 버그 등을 보내주세요!\n문의주신 내용은 개발팀 메일로 전송됩니다.",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black,
+                      ),
+                    ),
+                    margin: EdgeInsets.only(bottom: 10),
                   ),
-                ),
-                CommonTextField(onTap: () {})
-              ],
+                  CommonTextField(
+                    allowImages: false,
+                    onTap: sendEmail,
+                  )
+                ],
+              ),
             ),
           ),
         )
       ],
     );
   }
+}
+
+Future<void> sendEmail({Map<String, String> fields, dynamic files}) async {
 }
