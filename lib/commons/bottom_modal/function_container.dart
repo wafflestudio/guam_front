0c3812809144dfa00b2cc65bbde7ef14db8fb6f4
yeonghyeonKO <dk02315@gmail.com>
2021-08-13 11:11:42 +0900

    decorate alert message

--- lib/commons/bottom_modal/function_container.dart
+++ lib/commons/bottom_modal/function_container.dart
@@ -18,10 +18,22 @@ class FunctionContainer extends StatelessWidget {
     Future<void> _showMyDialog() async {
       return showDialog<void>(
         context: context,
-        barrierDismissible: false, // user must tap button!
+        barrierDismissible: false,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text("$text"),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20))
+            ),
+            titlePadding: EdgeInsets.all(0),
+            title: Container(
+              child: Text(""),
+              // child: Text("$text", style: TextStyle(color: Colors.white),),
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+              ),
+            ),
+            contentPadding: EdgeInsets.all(20),
             content: SingleChildScrollView(
               child: Column(
                 children: <Widget>[
@@ -31,14 +43,14 @@ class FunctionContainer extends StatelessWidget {
             ),
             actions: <Widget>[
               TextButton(
-                child: Text('확인'),
+                child: Text('확인', style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
                 onPressed: () {
                   customFunction();
                   Navigator.of(context).pop();
                 },
               ),
               TextButton(
-                child: Text('취소'),
+                child: Text('취소', style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold)),
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
