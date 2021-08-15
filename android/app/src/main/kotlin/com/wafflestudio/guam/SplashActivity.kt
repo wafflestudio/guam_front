c120303eebaf36196484a82cd8c7522df22d251e
gajagajago <gajagajago@naver.com>
2021-08-15 19:22:28 +0900

    temp

---
+++ android/app/src/main/kotlin/com/wafflestudio/guam/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.wafflestudio.guam
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends Activity {
+    Animation anim_slide_fish;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
