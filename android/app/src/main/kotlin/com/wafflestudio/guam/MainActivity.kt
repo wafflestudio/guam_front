a4f2d87abaab0146c08dac39f2dda4cd9960dce8
gajagajago <gajagajago@naver.com>
2021-08-15 21:01:49 +0900

    somewhat un fluent

--- android/app/src/main/kotlin/com/wafflestudio/guam/MainActivity.kt
+++ android/app/src/main/kotlin/com/wafflestudio/guam/MainActivity.kt
@@ -1,6 +1,34 @@
 package com.wafflestudio.guam
 
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import io.flutter.embedding.android.FlutterActivity
 
+//import androidx.appcompat.app.AppCompatActivity
+
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val iv = findViewById<ImageView>(R.id.iv)
+        val anim = AnimationUtils.loadAnimation(this, R.anim.anim_slide_fish).also {
+            it.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+
+                override fun onAnimationRepeat(animation: Animation?) {}
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    //context 전환
+                    startActivity(
+                            FlutterActivity.createDefaultIntent(this@MainActivity)
+                    )
+                }
+            })
+        }
+
+        iv.startAnimation(anim)
+    }
 }
