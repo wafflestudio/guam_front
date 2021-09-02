8f0e544c64badaf6566edc144cd76e992feb255a
gajagajago <gajagajago@naver.com>
2021-09-03 01:21:37 +0900

    show tutorial code on Home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../providers/home/home_provider.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      showTutorial();
+    });
+  }
+
+  Future<void> showTutorial() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool seenTutorial = prefs.getBool("seenTutorial") ?? false;
+
+    if (seenTutorial) return; // No need to show tutorial twice.
+
+    // code for showing tutorial
+
+    // tutorial 마지막 button press 시 -> await prefs.setBool("seenTutorial", true); 해주심 됩니다.
+  }
+
   @override
   Widget build(BuildContext context) {
     final homeProvider = context.watch<HomeProvider>();
