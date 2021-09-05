4a7416d92b36dffe7c7f70474dcbbc35c0be10e7
yeonghyeonKO <dk02315@gmail.com>
2021-09-05 18:39:56 +0900

    not to keep rendering tips when searching projects

--- lib/screens/projects/search/projects_searched_list.dart
+++ lib/screens/projects/search/projects_searched_list.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import '../project_banner.dart';
 import '../sub_headings.dart';
 import 'package:provider/provider.dart';
 import '../../../providers/projects/projects.dart';
 
+final List<String> tips = [
+  "Tip : 키워드와 더불어 필터링을 활용해보세요.",
+  "Tip : 원하는 기술 스택이 없다면 ETC를 선택해주세요.",
+  "Tip : 프로젝트는 최대 3개까지 참여할 수 있습니다.",
+];
+final random = new Random();
+
 class ProjectsSearchedList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final List<String> tips = [
-      "Tip : 키워드와 더불어 필터링을 활용해보세요.",
-      "Tip : 원하는 기술 스택이 없다면 ETC를 선택해주세요.",
-    ];
-    tips.shuffle();
-
     Projects projectsProvider = context.watch<Projects>();
 
     return projectsProvider.loading ? CircularProgressIndicator() : Column(
@@ -34,7 +36,10 @@ class ProjectsSearchedList extends StatelessWidget {
                 )
             : Padding(
                 padding: EdgeInsets.only(top: 50),
-                child: Text(tips.first, style: TextStyle(fontSize: 15)),
+                child: Text(
+                  tips[random.nextInt(tips.length)],
+                  style: TextStyle(fontSize: 15),
+                ),
               ),
         )
       ],
