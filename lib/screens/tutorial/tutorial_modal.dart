5dd4104180eaadf6f26f769bf6ac448fb4f478a8
yeonghyeonKO <dk02315@gmail.com>
2021-09-07 20:55:23 +0900

    fix tutorial page

--- lib/screens/tutorial/tutorial_modal.dart
+++ lib/screens/tutorial/tutorial_modal.dart
@@ -19,71 +19,46 @@ class _TutorialModalState extends State<TutorialModal> {
   Widget build(BuildContext context) {
     return Dialog(
       backgroundColor: Colors.transparent,
-      insetPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
       child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             if (_currentPage == 1)
               TutorialPage(
-                title: "프로젝트 참여",
-                description_1: "현재 모집중인 프로젝트를",
-                description_2: "손쉽게 찾을 수 있어요",
-                imagePath: "assets/tutorial/tutorial_project_tab.png",
-                isImageTop: false,
+                imagePath: "assets/tutorial/tutorial-1.png",
                 page: _currentPage,
                 goToNextPage: goToNextPage
               ),
             if (_currentPage == 2)
               TutorialPage(
-                title: "프로젝트 검색",
-                description_1: "기술스택, 포지션, 기간별",
-                description_2: "프로젝트를 찾을 수 있어요",
-                imagePath: "assets/tutorial/tutorial_project_search.png",
-                isImageTop: false,
+                imagePath: "assets/tutorial/tutorial-2.png",
                 page: _currentPage,
                 goToNextPage: goToNextPage,
                 goToPreviousPage: goToPreviousPage,
               ),
             if (_currentPage == 3)
               TutorialPage(
-                title: "프로젝트 생성",
-                description_1: "기술 스택과 인원을 고려하여",
-                description_2: "프로젝트를 만들 수 있어요",
-                imagePath: "assets/tutorial/tutorial_project_create.png",
-                isImageTop: true,
+                imagePath: "assets/tutorial/tutorial-3.png",
                 page: _currentPage,
                 goToNextPage: goToNextPage,
                 goToPreviousPage: goToPreviousPage,
               ),
             if (_currentPage == 4)
               TutorialPage(
-                title: "작업실",
-                description_1: "작업실에서 협업에 필요한",
-                description_2: "다양한 기능을 만나보세요",
-                imagePath: "assets/tutorial/tutorial_board.png",
-                isImageTop: false,
+                imagePath: "assets/tutorial/tutorial-4.png",
                 page: _currentPage,
                 goToNextPage: goToNextPage,
                 goToPreviousPage: goToPreviousPage,
               ),
             if (_currentPage == 5)
               TutorialPage(
-                title: "스레드",
-                description_1: "팀원들과 소통이 필요한 경우",
-                description_2: "스레드에 글을 남길 수 있어요",
-                imagePath: "assets/tutorial/tutorial_thread.png",
-                isImageTop: true,
+                imagePath: "assets/tutorial/tutorial-5.png",
                 page: _currentPage,
                 goToNextPage: goToNextPage,
                 goToPreviousPage: goToPreviousPage,
               ),
             if (_currentPage == 6)
               TutorialPage(
-                title: "프로필",
-                description_1: "프로필에서 자신이 사용가능한",
-                description_2: "기술 스택을 소개할 수 있어요",
-                imagePath: "assets/tutorial/tutorial_profile_tab.png",
-                isImageTop: false,
+                imagePath: "assets/tutorial/tutorial-6.png",
                 page: _currentPage,
                 goToNextPage: goToNextPage,
                 goToPreviousPage: goToPreviousPage,
